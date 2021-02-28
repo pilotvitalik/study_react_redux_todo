a97707675bc3b4698bc050a1da3d84b5dd28d4e4
Vitaliy <itvkip@yandex.ru>
2021-02-28 22:14:41 +0300

    - added module Material-UI; - added settings for corect displaying Material-UI;

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
-import axios from 'axios';
-
 function App() {
-
-	axios.get('http://127.0.0.1:3030/')
-	  .then(res=>console.log(res))
-	  .catch(err=>console.log(err))
-
   return (
     <div className="App">
       TODO appliction
