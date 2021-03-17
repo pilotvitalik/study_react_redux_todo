f8cb6f645fd03dceac080eba06f8fbd2b245c6ac
Vitaliy <itvkip@yandex.ru>
2021-03-17 21:59:56 +0300

    Added styles for checkboxes in the component "Filter by colors".

--- src/Components/Todo/AdditionalParams/FilterColor/FilterColor.js
+++ src/Components/Todo/AdditionalParams/FilterColor/FilterColor.js
@@ -4,27 +4,60 @@ import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
-import { withStyles } from '@material-ui/core/styles';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 
 const useStyles = makeStyles((theme) => ({
-	green:{
-		width: '50px',
-		height: '10px',
+	root:{
+		paddingRight: '4px',
 	},
+	common:{
+		width: '20px',
+		height: '12px',
+		borderRadius: '4px',
+	},
+	green:{
+		background: 'green',
+	},
+	blue:{
+		background: 'blue',
+	},
+	orange:{
+		background: 'orange',
+	},
+	purple:{
+		background: 'purple',
+	},
+	red:{
+		background: 'red',
+	},
+	iconGreen:{
+		'& input:checked + svg':{
+			color: 'green',
+		}
+	},
+	iconBlue:{
+		'& input:checked + svg':{
+			color: 'blue',
+		}
+	},
+	iconOrange:{
+		'& input:checked + svg':{
+			color: 'orange',
+		}
+	},
+	iconPurple:{
+		'& input:checked + svg':{
+			color: 'purple',
+		}
+	},
+	iconRed:{
+		'& input:checked + svg':{
+			color: 'red',
+		}
+	}
 }))
-
-const GreenCheckbox = withStyles({
-  root: {
-  	root: '#767676',
-    '&$checked': {
-      color: 'red',
-    },
-  },
-  checked: {},
-})((props) => <Fragment><Box {...props}></Box><Checkbox {...props} /></Fragment>);
 
 function FilterColor(){
 	const classes = useStyles();
@@ -44,12 +77,83 @@ function FilterColor(){
 	return(
 		<Grid item xs={6}>
 		  <Typography variant={'subtitle1'} align={'center'}><b>Фильтр по цветам</b></Typography>
-		  <FormGroup row>
-		    <FormControlLabel control={<GreenCheckbox checked={state.checkedGreen} onChange={handleChange} name="checkedGreen" className={classes.green}/>} label="Green"/>
-		    <FormControlLabel control={<GreenCheckbox checked={state.checkedBlue} onChange={handleChange} name="checkedBlue" />} label="Blue"/>
-		    <FormControlLabel control={<GreenCheckbox checked={state.checkedOrange} onChange={handleChange} name="checkedOrange" />} label="Orange"/>
-		    <FormControlLabel control={<GreenCheckbox checked={state.checkedPurple} onChange={handleChange} name="checkedPurple" />} label="Purple"/>
-		    <FormControlLabel control={<GreenCheckbox checked={state.checkedRed} onChange={handleChange} name="checkedRed" />} label="Red"/>
+		  <FormGroup>
+		    <FormControlLabel control={
+		    	<Fragment>
+		    		<Checkbox
+		    			checked={state.checkedGreen}
+		    			onChange={handleChange}
+		    			className={[classes.root, classes.iconGreen].join(' ')}
+		    			name="checkedGreen"
+		    		/>
+		    		<Box
+		    			className={[classes.common, classes.green].join(' ')}
+		    			mr={0.5}
+		    		></Box>
+		    		</Fragment>
+		    	}
+		    	label="Green"
+		    	/>
+		    <FormControlLabel control={
+		    	<Fragment>
+		    		<Checkbox
+		    			checked={state.checkedBlue}
+		    			onChange={handleChange}
+		    			className={[classes.root, classes.iconBlue].join(' ')}
+		    			name="checkedBlue"
+		    		/>
+		    		<Box
+		    			className={[classes.common, classes.blue].join(' ')}
+		    			mr={0.5}
+		    		></Box>
+		    		</Fragment>
+		    	}
+		    	label="Blue"/>
+		    <FormControlLabel control={
+		    	<Fragment>
+		    		<Checkbox
+		    			checked={state.checkedOrange}
+		    			onChange={handleChange}
+		    			className={[classes.root, classes.iconOrange].join(' ')}
+		    			name="checkedOrange"
+		    		/>
+		    		<Box
+		    			className={[classes.common, classes.orange].join(' ')}
+		    			mr={0.5}
+		    		></Box>
+		    		</Fragment>
+		    	}
+		    	label="Orange"/>
+		    <FormControlLabel control={
+		    	<Fragment>
+		    		<Checkbox
+		    			checked={state.checkedPurple}
+		    			onChange={handleChange}
+		    			className={[classes.root, classes.iconPurple].join(' ')}
+		    			name="checkedPurple"
+		    		/>
+		    		<Box
+		    			className={[classes.common, classes.purple].join(' ')}
+		    			mr={0.5}
+		    		></Box>
+		    		</Fragment>
+		    	}
+		    	label="Purple"/>
+		    <FormControlLabel control={
+		    	<Fragment>
+		    		<Checkbox
+		    			checked={state.checkedRed}
+		    			onChange={handleChange}
+		    			className={[classes.root, classes.iconRed].join(' ')}
+		    			name="checkedRed"
+		    		/>
+		    		<Box
+		    			className={[classes.common, classes.red].join(' ')}
+		    			mr={0.5}
+		    		></Box>
+		    		</Fragment>
+		    	}
+		    	label="Red"/>
 		  </FormGroup>
 		</Grid>
 	);
