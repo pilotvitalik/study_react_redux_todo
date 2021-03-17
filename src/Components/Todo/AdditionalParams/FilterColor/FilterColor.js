82b2b87ee38c7a0058bf413d0cad4aa9a01c6f1f
Vitaliy <itvkip@yandex.ru>
2021-03-17 17:40:29 +0300

    Added checkboxed in filterr by colors component;

--- src/Components/Todo/AdditionalParams/FilterColor/FilterColor.js
+++ src/Components/Todo/AdditionalParams/FilterColor/FilterColor.js
@@ -1,30 +1,56 @@
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import FormControl from '@material-ui/core/FormControl';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import { makeStyles } from '@material-ui/core/styles';
 
+import { withStyles } from '@material-ui/core/styles';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
+const useStyles = makeStyles((theme) => ({
+	green:{
+		width: '50px',
+		height: '10px',
+	},
+}))
+
+const GreenCheckbox = withStyles({
+  root: {
+  	root: '#767676',
+    '&$checked': {
+      color: 'red',
+    },
+  },
+  checked: {},
+})((props) => <Fragment><Box {...props}></Box><Checkbox {...props} /></Fragment>);
+
 function FilterColor(){
-	const [value, setValue] = useState('female');
+	const classes = useStyles();
+
+	const [state, setState] = useState({
+	    checkedGreen: false,
+	    checkedBlue: false,
+	    checkedOrange: false,
+	    checkedPurple: false,
+	    checkedRed: false,
+	  });
 
 	const handleChange = (event) => {
-	  setValue(event.target.value);
-	};
+	    setState({ ...state, [event.target.name]: event.target.checked });
+	  };
 
 	return(
 		<Grid item xs={6}>
 		  <Typography variant={'subtitle1'} align={'center'}><b>Фильтр по цветам</b></Typography>
-		  <FormControl component="fieldset">
-		    <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
-		      <FormControlLabel value="female" control={<Radio />} label="Female" />
-		      <FormControlLabel value="male" control={<Radio />} label="Male" />
-		      <FormControlLabel value="other" control={<Radio />} label="Other" />
-		      <FormControlLabel value="disabled" disabled control={<Radio />} label="(Disabled option)" />
-		    </RadioGroup>
-		  </FormControl>
+		  <FormGroup row>
+		    <FormControlLabel control={<GreenCheckbox checked={state.checkedGreen} onChange={handleChange} name="checkedGreen" className={classes.green}/>} label="Green"/>
+		    <FormControlLabel control={<GreenCheckbox checked={state.checkedBlue} onChange={handleChange} name="checkedBlue" />} label="Blue"/>
+		    <FormControlLabel control={<GreenCheckbox checked={state.checkedOrange} onChange={handleChange} name="checkedOrange" />} label="Orange"/>
+		    <FormControlLabel control={<GreenCheckbox checked={state.checkedPurple} onChange={handleChange} name="checkedPurple" />} label="Purple"/>
+		    <FormControlLabel control={<GreenCheckbox checked={state.checkedRed} onChange={handleChange} name="checkedRed" />} label="Red"/>
+		  </FormGroup>
 		</Grid>
 	);
 }
